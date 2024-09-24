d141749f82929302e59a4962b0e04ce1ffe5345b
Yuri <xamexavu@hotmail.com>
2024-09-24 12:26:36 -0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,45 +1,70 @@
 import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, UnexpectedAlertPresentException, NoAlertPresentException
-import time
-from selenium.webdriver.common.keys import Keys
+from time import sleep
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+# Definir a URL da postagem de interesse ou busca no Twitter
+query_url = 'https://twitter.com/search?q=vacina%20HPV%20OR%20vacina%20de%20HPV&src=typed_query&f=live'
 
+# Configuração do WebDriver para Chrome
 chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-wait = WebDriverWait(driver, 15)
+# Inicializar o driver
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+wait = WebDriverWait(driver, 10)
 
-# URL de busca no Twitter/X
-url = 'https://twitter.com/search?q=vacina%20HPV%20OR%20vacina%20de%20HPV&src=typed_query&f=live'
+# Função de login (caso seja necessário)
+def login():
+    driver.get('https://x.com/i/flow/login')
+    email_input = wait.until(EC.presence_of_element_located((By.NAME, "text")))
+    email_input.send_keys(os.getenv("TWITTER_EMAIL"))
+    
+    next_btn = driver.find_element(By.CSS_SELECTOR, 'button span span')
+    next_btn.click()
+    sleep(3)
 
-# Acessar a página
-driver.get(url)
-time.sleep(5)  # Espera a página carregar
+    password_input = wait.until(EC.presence_of_element_located((By.NAME, "password")))
+    password_input.send_keys(os.getenv("TWITTER_PASSWORD"))
 
-# Extrair tweets
-tweets = driver.find_element(By.CSS_SELECTOR, "article")
+    login_btn = driver.find_element(By.CSS_SELECTOR, 'button span span')
+    login_btn.click()
+    sleep(5)
 
-for tweet in tweets:
-    print(tweet.text)
+# Função para buscar e coletar os comentários
+def scrape_tweets():
+    driver.get(query_url)
+    sleep(3)
+    
+    tweets = []
+    
+    for _ in range(5):  # Loop para rolar a página e carregar mais tweets
+        elements = driver.find_elements(By.XPATH, '//div[@data-testid="tweetText"]')
+        for el in elements:
+            tweets.append(el.text)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(2)  # Aguardar enquanto a página carrega
+    
+    return tweets
 
+# Realizar scraping
+tweets = scrape_tweets()
+
+# Salvar os resultados em um arquivo
+with open('tweets_hp_vacina.csv', 'w') as file:
+    for tweet in tweets:
+        file.write(f"{tweet}\n")
+
+# Fechar o driver
 driver.quit()
+
+print(f"Coletados {len(tweets)} tweets.")
