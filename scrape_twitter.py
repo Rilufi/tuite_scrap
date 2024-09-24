ea7d206b80fdc25b3c8017ab12413315760496cc
Yuri <xamexavu@hotmail.com>
2024-09-24 12:45:14 -0300

    Update scrape_twitter.py

--- scrape_twitter.py
+++ scrape_twitter.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+import os
 
 # Configuração do WebDriver
 chrome_options = Options()
