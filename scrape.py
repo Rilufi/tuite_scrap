0e0b20b8783435ae49799d3958afd83c95d9077f
Yuri <xamexavu@hotmail.com>
2024-09-24 11:50:40 -0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, UnexpectedAlertPresentException, NoAlertPresentException
-from time import sleep
+import time
 from selenium.webdriver.common.keys import Keys
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
