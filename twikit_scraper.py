ccba1f6c04eb7023c8b248dd6d2078dc6ce12ccf
Yuri <xamexavu@hotmail.com>
2024-09-24 14:34:29 -0300

    Update twikit_scraper.py

--- twikit_scraper.py
+++ twikit_scraper.py
@@ -1,5 +1,6 @@
 from twikit import Client
 import os
+import asyncio
 
 USERNAME = os.getenv("TWITTER_USERNAME")
 EMAIL = os.getenv("TWITTER_EMAIL")
@@ -8,16 +9,16 @@ PASSWORD = os.getenv("TWITTER_PASSWORD")
 # Initialize client
 client = Client('en-US')
 
-def main():
-   client.login(
+async def main():
+   await client.login(
         auth_info_1=USERNAME ,
         auth_info_2=EMAIL,
         password=PASSWORD
     )
 
-main()
+asyncio.run(main())
 
-tweets = client.search_tweet('vacina', 'hpv')
+tweets = await client.search_tweet('vacina hpv', 'Latest')
 
 for tweet in tweets:
     print(
