876eedb33cdc97785c4ed65228fdfbf31ebfdbb0
Yuri <xamexavu@hotmail.com>
2024-09-25 16:09:15 -0300

    Create get_cookie.py

---
+++ get_cookie.py
@@ -0,0 +1,13 @@
+from twikit import Client, TooManyRequests
+from configparser import ConfigParser
+
+#* login credentials
+username = os.getenv("TWITTER_USERNAME")
+email = os.getenv("TWITTER_EMAIL")
+password = os.getenv("TWITTER_PASSWORD")
+
+#* authenticate to X.com
+#! 1) use the login credentials. 2) use cookies.
+client = Client(language='en-US')
+client.login(auth_info_1=username, auth_info_2=email, password=password)
+client.save_cookies('cookies.json')
