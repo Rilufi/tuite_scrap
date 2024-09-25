0ac26f238edb9b7b8d7580d609b81dbd6dedfb4b
Yuri <xamexavu@hotmail.com>
2024-09-25 18:26:06 -0300

    Update get_cookie.py

--- get_cookie.py
+++ get_cookie.py
@@ -1,73 +1,16 @@
-from twikit import Client, TooManyRequests
-import time
-from datetime import datetime
+import snscrape.modules.twitter as sntwitter
 import csv
-from configparser import ConfigParser
-from random import randint
-import os
 
-MINIMUM_TWEETS = 10
-QUERY = 'vacina hpv lang:pt until:2024-25-09 since:2024-25-09'
+query = 'vacina hpv lang:pt since:2024-09-25 until:2024-09-26'
 
-
-def get_tweets(tweets):
-    if tweets is None:
-        print(f'{datetime.now()} - Getting tweets...')
-        tweets = client.search_tweet(QUERY, product='Top')
-    else:
-        wait_time = randint(5, 10)
-        print(f'{datetime.now()} - Getting next tweets after {wait_time} seconds ...')
-        time.sleep(wait_time)
-        tweets = tweets.next()
-
-    return tweets
-
-
-#* login credentials
-username = os.getenv("TWITTER_USERNAME")
-email = os.getenv("TWITTER_EMAIL")
-password = os.getenv("TWITTER_PASSWORD")
-
-#* create a csv file
-with open('tweets.csv', 'w', newline='') as file:
+# Create a CSV file to store the tweets
+with open('tweets.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(['Tweet_count', 'Username', 'Text', 'Created At', 'Retweets', 'Likes'])
 
-#* authenticate to X.com
-client = Client(language='en-US')
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
+        if i > 10:  # Adjust this limit according to your needs
+            break
+        writer.writerow([i+1, tweet.user.username, tweet.content, tweet.date, tweet.retweetCount, tweet.likeCount])
 
-# Uncomment to save cookies after logging in
-client.login(auth_info_1=username, auth_info_2=email, password=password)
-client.save_cookies('cookies.json')
-
-# Once cookies are saved, use them for further executions
-# client.load_cookies('cookies.json')
-
-tweet_count = 0
-tweets = None
-
-while tweet_count < MINIMUM_TWEETS:
-    try:
-        tweets = get_tweets(tweets)
-    except TooManyRequests as e:
-        rate_limit_reset = datetime.fromtimestamp(e.rate_limit_reset)
-        print(f'{datetime.now()} - Rate limit reached. Waiting until {rate_limit_reset}')
-        wait_time = rate_limit_reset - datetime.now()
-        time.sleep(wait_time.total_seconds())
-        continue
-
-    if not tweets:
-        print(f'{datetime.now()} - No more tweets found')
-        break
-
-    for tweet in tweets:
-        tweet_count += 1
-        tweet_data = [tweet_count, tweet.user.name, tweet.text, tweet.created_at, tweet.retweet_count, tweet.favorite_count]
-
-        with open('tweets.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(tweet_data)
-
-    print(f'{datetime.now()} - Got {tweet_count} tweets')
-
-print(f'{datetime.now()} - Done! Got {tweet_count} tweets')
+print(f'Done! Collected {i+1} tweets.')
